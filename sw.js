fdd6149d03a17a140c9b5742e93f1e523897a681
dylan djian <dylandjian@gmail.com>
2018-06-09 23:01:49 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-b79f9192562ac7b76d02.js","9eb2551208c43f111eaee40f58928f45"],["/commons-33a4ef356bd50d18692a.js","3d2321ce9b77dfcd9d65f588f5596cbd"],["/index.html","8eaf59266782b488b06fb44f5d1c5045"],["/offline-plugin-app-shell-fallback/index.html","19ef88723a160cff97c48da66c32032f"]];
+var precacheConfig = [["/app-b79f9192562ac7b76d02.js","9eb2551208c43f111eaee40f58928f45"],["/commons-33a4ef356bd50d18692a.js","3d2321ce9b77dfcd9d65f588f5596cbd"],["/index.html","7698566816104fb40cdaff4497974a99"],["/offline-plugin-app-shell-fallback/index.html","754ea3d572fde402f7b80c737635779b"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
